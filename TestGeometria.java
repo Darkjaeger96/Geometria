a9f11e7580a48cf360817b2a06f57e135a3a6a82
jesus villar <jesusonazo96@hotmail.com>
2017-11-02 13:48:54 +0100

    Commit 2 	modificado:    Geometria.java 	modificado:    TestGeometria.java
    	Añadidos métodos para calcular area del triangulo y area del circulo.

--- TestGeometria.java
+++ TestGeometria.java
@@ -7,7 +7,13 @@ public class TestGeometria {
 		System.out.println("Área del cuadrado de lado 2.123 vale " + areaCuadrado);
 		
 		double areaRectangulo = Geometria.calcularAreaRectangulo(2.123, 1.233);
-		System.out.println("Área del cuadrado de base 2.123 y altura 1.233 vale " + areaRectangulo);
+		System.out.println("Área del rectangulo de base 2.123 y altura 1.233 vale " + areaRectangulo);
+		
+		double areaTriangulo = Geometria.calcularAreaTriangulo(2.123, 1.233);
+		System.out.println("Área del triangulo de base 2.123 y altura 1.233 vale " + areaTriangulo);
+		
+		double areaCirculo = Geometria.calcularAreaCirculo(2.123);
+		System.out.println("Área del circulo de radio 2.123" + areaCirculo);
 	}
 }
 
